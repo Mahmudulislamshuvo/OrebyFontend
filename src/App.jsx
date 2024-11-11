2bd2a8a3744ac57bb332934090a734f7dbda0ba5
Mahmudul Islam <mahmudulislammern@gmail.com>
2024-11-12 02:16:10 +0600

    Routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={"this is main Route"} />
+      <Route path="/home" element={"this is home"} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-      <div>app</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      // padding: {
+      //   DEFAULT: "1rem",
+      //   sm: "2rem",
+      //   lg: "4rem",
+      //   xl: "5rem",
+      //   "2xl": "6rem",
+      // },
+    },
+    extend: {
+      colors: {
+        whiteColor: "var(--whiteColor)",
+        black_363738: "var(--black_363738)",
+        whitesmoke_F5F5F5: "var(--whitesmoke_F5F5F5)",
+        redOrange_FEFAF1: "var(--redOrange_FEFAF1)",
+        Snow_text1: "var(--Snow_text1)",
+        blackfull: "var(--blackfull)",
+        red_DB4444: "var(--red_DB4444)",
+        green_00FF66: "var(--green_00FF66)",
+        SoftRed_E07575: "var(--SoftRed_E07575)",
+        SoftBlue_A0BCE0: "var(--SoftBlue_A0BCE0)",
+      },
+    },
+    fontFamily: {
+      inter: ["Inter", "sans - serif"],
+      poppins: ["Poppins", "sans - serif"],
+    },
   },
   plugins: [],
 };
