99237ae1934cd9295aacd54b936ac79986569450
Mahmudul Islam <mahmudulislammern@gmail.com>
2024-12-22 03:25:20 +0600

    making redex toolkit rtk

--- src/Components/CommonComponents/CategoryCommon.jsx
+++ src/Components/CommonComponents/CategoryCommon.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const CategoryCommon = ({ ItemData }) => {
   return (
     <div className="h-[145px] w-[170px] rounded border-4 border-whitesmoke_F5F5F5">
-      <div className="flex h-full flex-col items-center justify-center text-center">
+      <div className="group flex h-full flex-col items-center justify-center text-center transition-all hover:rounded hover:border-0 hover:bg-red_DB4444 hover:text-whiteColor">
         <span className="inline-block text-center text-[60px]">
           {ItemData.img}
         </span>
-        <span className="inline-block pt-4 font-poppins text-base text-text2_black_full">
+        <span className="inline-block pt-4 font-poppins text-base text-text2_black_full group-hover:text-whiteColor">
           {ItemData.name}
         </span>
       </div>
--- src/Features/store.js
+++ src/Features/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./AllSlice/countSlice.js";
+import { productApi } from "../Features/Api/PtoductApi.js";
 
 export const store = configureStore({
   reducer: {
