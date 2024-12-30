b475b55a6011ef8e1bcd1419f99b936c1579a9ca
Mahmudul Islam <mahmudulislammern@gmail.com>
2024-12-31 03:06:27 +0600

    making design of order

--- src/Components/CommonComponents/ProductsDetails/ImageGallery.jsx
+++ src/Components/CommonComponents/ProductsDetails/ImageGallery.jsx
@@ -7,7 +7,7 @@ const ImageGallery = ({ image }) => {
   const SingleImages =
     image && image.length > 0
       ? image[0]
-      : "https://images.pexels.com/photos/29487999/pexels-photo-29487999/free-photo-of-seagulls-flying-over-istanbul-skyline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+      : "https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80";
   const [InitialImage, setInitialImage] = useState(SingleImages);
   console.log(InitialImage);
 
@@ -31,12 +31,12 @@ const ImageGallery = ({ image }) => {
           ))}
           <div className="rounded bg-whitesmoke_F5F5F5 px-6 py-3">
             <img
-              src="https://images.pexels.com/photos/29487999/pexels-photo-29487999/free-photo-of-seagulls-flying-over-istanbul-skyline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt="https://images.pexels.com/photos/29487999/pexels-photo-29487999/free-photo-of-seagulls-flying-over-istanbul-skyline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src="https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80"
+              alt="https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80"
               className="h-[114px] w-[121px] cursor-pointer object-contain"
               onClick={() =>
                 makeitBig(
-                  "https://images.pexels.com/photos/29487999/pexels-photo-29487999/free-photo-of-seagulls-flying-over-istanbul-skyline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                  "https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80",
                 )
               }
             />
--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ImageGallery from "../CommonComponents/ProductsDetails/ImageGallery";
 import { useGetsingleProductQuery } from "../../Features/Api/PtoductApi";
+import { FaStar } from "react-icons/fa6";
 
 const ProductDetails = () => {
   const { data, error, isLoading } = useGetsingleProductQuery(parseInt(1));
@@ -11,7 +12,49 @@ const ProductDetails = () => {
         <div className="mr-4 w-[60%]">
           <ImageGallery image={data?.images} />
         </div>
-        <div className="w-[40%] bg-red-300"></div>
+        <div className="w-[40%]">
+          <div>
+            <h3 className="pb-4 text-2xl font-semibold tracking-[3%] text-text2_black_full">
+              Havic HV G-92 Gamepad
+            </h3>
+            {/* stars and instock */}
+            <div className="flex items-center pb-4">
+              <div className="flex items-center gap-x-1 pr-2 text-yellow-400">
+                <span>
+                  <FaStar />
+                </span>
+                <span>
+                  <FaStar />
+                </span>
+                <span>
+                  <FaStar />
+                </span>
+                <span>
+                  <FaStar />
+                </span>
+              </div>
+              <span className="pr-4 text-text2_black_full opacity-50">
+                (150 Reviews)
+              </span>
+              <span className="pr-4 text-text2_black_full opacity-50">|</span>
+              <span className="font-poppins text-sm text-green_00FF66 opacity-60">
+                In Stock
+              </span>
+              {/* stars and instock end*/}
+            </div>
+            <div className="border-b-[2px] border-[rgba(0,0,0,0.5)]">
+              <h4 className="pb-6 font-inter text-2xl tracking-[3%] text-text2_black_full">
+                $192.00
+              </h4>
+              <p className="pb-6 font-poppins text-sm text-text2_black_full">
+                PlayStation 5 Controller Skin High quality vinyl with air
+                channel adhesive for easy bubble free install & mess free
+                removal Pressure sensitive.
+              </p>
+            </div>
+            <div>jkgug</div>
+          </div>
+        </div>
       </div>
     </div>
   );
