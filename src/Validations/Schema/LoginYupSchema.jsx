68ec493282257a6b30496661a2a50f5d103f3c9e
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-01-06 01:41:40 +0600

    regex okay now

--- src/Validations/Schema/LoginYupSchema.jsx
+++ src/Validations/Schema/LoginYupSchema.jsx
@@ -1,13 +1,13 @@
 import * as Yup from "yup";
 
-let mailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+let mailFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 let passwordFormat =
   /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
 const bdPhoneNumberRegex = /^(?:\+?88)?01[3-9]\d{8}$/;
 
 export const LoginYupSchema = Yup.object({
   emailorPhone: Yup.string()
-    .matches("Invalid email format", mailFormat)
+    .matches(mailFormat, "Invalid email format")
     .required("Email Missing!!"),
   password: Yup.string()
     .matches(
